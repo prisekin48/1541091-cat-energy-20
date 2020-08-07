10c1cdfa835e7b26fc694f5fc4104bdefcf488b3
Prisekin Alexandr <aprisekin48@yandex.ru>
2020-08-07 17:55:26 +0300

    Исправления для Тревис

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,9 @@
 var nav = document.querySelector('.nav');
 var menuButton = document.querySelector('.header__menu-button');
 
- if (window.innerWidth < 768) {
-   nav.classList.add('visually-hidden');
- }
+if (window.innerWidth < 768) {
+ nav.classList.add('visually-hidden');
+}
 
 menuButton.classList.remove('header__menu-button--nojs');
 
