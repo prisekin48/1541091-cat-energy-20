5a5809e5aa26d91d4467f1802c660952cd9f848e
Prisekin Alexandr <aprisekin48@yandex.ru>
2020-08-12 19:06:31 +0300

    Исправлен js отображения навигации

--- source/js/app.js
+++ source/js/app.js
@@ -1,25 +1,11 @@
 var nav = document.querySelector('.nav');
 var menuButton = document.querySelector('.header__menu-button');
 
-if (window.innerWidth < 768) {
-  nav.classList.add('visually-hidden');
-}
-
 menuButton.classList.remove('header__menu-button--nojs');
 
-
 menuButton.addEventListener('click', function() {
-  if (menuButton.classList.contains('header__menu-button--nav-closed')) {
-    menuButton.classList.remove('header__menu-button--nav-closed');
-    menuButton.classList.add('header__menu-button--nav-opened');
-    nav.classList.remove('visually-hidden');
-  } else {
-    menuButton.classList.add('header__menu-button--nav-closed');
-    menuButton.classList.remove('header__menu-button--nav-opened');
-    nav.classList.add('visually-hidden');
-  }
+  menuButton.classList.toggle('header__menu-button--nav-closed');
 });
-
 
 var sampleButton = document.querySelector('.sample__button');
 var sampleImage = document.querySelector('.sample__image');
