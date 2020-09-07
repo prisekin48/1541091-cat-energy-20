271dfb5be962eb9432f6ee2e0dcdba5aec443acd
prisekin48 <prisekin48@gmail.com>
2020-09-07 21:47:20 +0300

    Правки js

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,9 @@
 let nav = document.querySelector('.nav');
 let menuButton = document.querySelector('.header__menu-button');
 let map = document.querySelector('.welcome__map');
+let sampleCatsWrapper = document.querySelector('.sample__cats-wrapper');
+let sampleStateTextBefore = document.querySelector('.sample__state-text--before');
+let sampleStateTextAfter = document.querySelector('.sample__state-text--after');
 
 menuButton.classList.add('header__menu-button--nav-closed', 'header__menu-button--js');
 map.classList.remove('welcome__map--nojs');
@@ -10,31 +13,32 @@ menuButton.addEventListener('click', function() {
 });
 
 let sampleButton = document.querySelector('.sample__button');
-let sampleCatsWrapper = document.querySelector('.sample__cats-wrapper');
 
-sampleButton.addEventListener('click', function() {
-  sampleButton.classList.toggle('sample__button--after');
-  sampleCatsWrapper.classList.toggle('sample__cats-wrapper--after');
-});
+if (sampleButton) {
+  sampleButton.addEventListener('click', function() {
+    sampleButton.classList.toggle('sample__button--after');
+    sampleCatsWrapper.classList.toggle('sample__cats-wrapper--after');
+  });
+};
 
-let sampleStateTextBefore = document.querySelector('.sample__state-text--before');
-let sampleStateTextAfter = document.querySelector('.sample__state-text--after');
-
+if (sampleStateTextBefore) {
 sampleStateTextBefore.addEventListener('click', function() {
   sampleButton.classList.remove('sample__button--after');
   sampleCatsWrapper.classList.remove('sample__cats-wrapper--after');
 });
+};
 
+if (sampleStateTextAfter) {
 sampleStateTextAfter.addEventListener('click', function() {
   sampleButton.classList.add('sample__button--after');
   sampleCatsWrapper.classList.add('sample__cats-wrapper--after');
 });
-
+};
 
 
 function initMap() {
   let address = {lat: 59.938635, lng: 30.323118};
-  let icon = "../img/map-pin.png";
+  let icon = "./img/map-pin.png";
 
   map = new google.maps.Map(document.querySelector(".welcome__map"), {
     center: address,
